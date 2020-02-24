1cf8539f936a75aea2e19fcfc68d270ae1c8540c
Hung <hung.nguyen@saleshub.vn>
2020-02-24 15:45:37 +0700

    Coding to interfaces

--- src/TennisBookings.Web/Services/AmazingWeatherForecaster.cs
+++ src/TennisBookings.Web/Services/AmazingWeatherForecaster.cs
@@ -1,13 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace TennisBookings.Web.Services
 {
-    public class WeatherForecaster : IWeatherForecaster
+    public class AmazingWeatherForecaster : IWeatherForecaster
     {
         public WeatherResult GetCurrentWeather()
         {
-            // Pretend we call out to a remote 3rd party API here to get the real forecast!
-            // For demo purposes, the result is hard-coded.
-
             return new WeatherResult
             {
                 WeatherCondition = WeatherCondition.Sun
