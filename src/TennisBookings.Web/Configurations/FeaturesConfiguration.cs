155906f3acb87db422298f378d7f9f90aa4a3925
Hung <hung.nguyen@saleshub.vn>
2020-02-26 11:18:38 +0700

    Modify

---
+++ src/TennisBookings.Web/Configurations/FeaturesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisBookings.Web.Configurations
+{
+    public class FeaturesConfiguration
+    {
+        public bool EnableWeatherForecast { get; set; }
+    }
+}
--- src/TennisBookings.Web/Controllers/HomeController.cs
+++ src/TennisBookings.Web/Controllers/HomeController.cs
@@ -7,11 +7,14 @@ namespace TennisBookings.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IWeatherForecaster _weatherForecaster;
+        private readonly FeaturesConfiguration _featuresConfiguration;
 
         public HomeController(
-            IWeatherForecaster weatherForecaster)
+            IWeatherForecaster weatherForecaster,
+            IOptions<FeaturesConfiguration> featuresConfiguration)
         {
             _weatherForecaster = weatherForecaster;
+            _featuresConfiguration = featuresConfiguration.Value;
         }
 
         [Route("")]
@@ -19,22 +22,25 @@ namespace TennisBookings.Web.Controllers
         {
             var viewModel = new HomeViewModel();
 
-            var currentWeather = _weatherForecaster.GetCurrentWeather();
-            
-            switch (currentWeather.WeatherCondition)
+            if (_featuresConfiguration.EnableWeatherForecast)
             {
-                case WeatherCondition.Sun:
-                    viewModel.WeatherDescription = "It's sunny right now. " +
-                                                   "A great day for tennis.";
-                    break;
-                case WeatherCondition.Rain:
-                    viewModel.WeatherDescription = "We're sorry but it's raining " +
-                                                   "here. No outdoor courts in use.";
-                    break;
-                default:
-                    viewModel.WeatherDescription = "We don't have the latest weather " +
-                                                   "information right now, please check again later.";
-                    break;
+                var currentWeather = _weatherForecaster.GetCurrentWeather();
+
+                switch (currentWeather.WeatherCondition)
+                {
+                    case WeatherCondition.Sun:
+                        viewModel.WeatherDescription = "It's sunny right now. " +
+                                                       "A great day for tennis.";
+                        break;
+                    case WeatherCondition.Rain:
+                        viewModel.WeatherDescription = "We're sorry but it's raining " +
+                                                       "here. No outdoor courts in use.";
+                        break;
+                    default:
+                        viewModel.WeatherDescription = "We don't have the latest weather " +
+                                                       "information right now, please check again later.";
+                        break;
+                }
             }
 
             return View(viewModel);
